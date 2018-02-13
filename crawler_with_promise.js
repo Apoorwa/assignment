0d053aaa32ba19960eb3893995a9a3d2d3545be9
Apoorwa <apoorwa.mishra@gmail.com>
2018-02-13 14:57:33 +0530

    Updated the script crawler_with_promise to search recursively

--- crawler_with_promise.js
+++ crawler_with_promise.js
@@ -1,44 +1,67 @@
 var request = require('request');
 var cheerio = require('cheerio');
 var Promise = require('bluebird');
+var validator = require('valid-url');
 var fs = require('fs');
 var stringify = require('csv-stringify');
 
-var START_URL = "http://www.medium.com/";
-var urls = [];
-urls.push(START_URL);
-var visitedUrls = {}; //To avoid checking the same page again and again
-var maxConnections = 5;
-var getPage = function(url){
 
-  return new Promise(function(resolve,reject){
-    visitedUrls[url] = true;
-    request(url, function(err,res,body){
-      if(!err && res.statusCode === 200){
-        var $ = cheerio.load(body);
-        $('a').each(function(){
-          var url = $(this).attr('href');
-          // console.log(url);
-          if(url in visitedUrls){
-            console.log("This url is already been visited : ", url);
-          }
-          else{
-              urls.push(url);
-          }
-        });
+var urls= [];
+var allUrls = [];
+var visitedUrls = {};
+var START_URL = 'http://www.medium.com';
+urls.push(START_URL);
+allUrls.push(START_URL);
+const maxConnections = 5;
+
+var fetchPageLinks = function(url){
+  console.log("fetching page : ", url);
+  return new Promise(function(resolve, reject){
+    if(validator.isUri(url)){
+      if(url in visitedUrls){
         resolve(urls);
       }
       else{
-        reject(err);
+        visitedUrls[url] = true;
+        urls.splice(urls.indexOf(url),1);
+        request(url,function(err,res){
+          if(!err && res.statusCode === 200){
+            var $ = cheerio.load(res.body);
+            $('a').each(function(){
+              var url = $(this).attr('href');
+              if(url in visitedUrls){
+                // console.log(url + " skipping as already visited");
+              }
+              else{
+                urls.push(url);
+                allUrls.push(url);
+              }
+            });
+            resolve(urls);
+          }
+          else{
+            reject(err);
+          }
+        });
       }
-    });
+    }
+    else{
+      urls.splice(urls.indexOf(url),1);
+      resolve(urls)
+    };
   });
 }
 
-var crawl = function(url){
-  Promise.map(urls, getPage, {concurrency : maxConnections}).then(function(urls){
+
+var crawl = function(urls){
+  Promise.map(urls, fetchPageLinks, {concurrency: maxConnections}).then(function(result){
+    if(result[0].length){
+      console.log(result[0].length);
+      return crawl(result[0]);
+    }
+  }).finally(function(){
     let myObj = {};
-    myObj['urls'] = urls;
+    myObj['urls'] = allUrls;
     stringify(myObj.urls, function(err, output) {
       fs.writeFile('./data/urls.csv', output, 'utf8', function(err) {
         if (err) {
@@ -50,9 +73,9 @@ var crawl = function(url){
     });
   }).catch(function(err){
     if(err){
-      console.log("error happened : ", err, "\n");
+      console.log("Error : ", err);
     }
   });
 }
 
-crawl(START_URL);
+crawl(urls);
